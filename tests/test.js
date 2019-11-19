878815f06f17a9c8aadf894ead82e1f18cb19b22
Nektarios <nek.liakis@gmail.com>
2019-11-19 16:47:04 +0200

    Added methods for: teams, player, matches, competition, areas

--- tests/test.js
+++ tests/test.js
@@ -3,15 +3,5 @@ require('dotenv').config();
 const FootballDataApi = require('../lib/FootballDataApi');
 const util = require('util')
 
-// console.log(util.inspect(error, false, null, true /* enable colors */))
-//const footballDataApiKey = 'YOUR-API-KEY';
 const footballDataApiKey = process.env.FOOTBALL_DATA_APIKEY;
 const fd = new FootballDataApi(footballDataApiKey)
-
-fd.getCompetitions().then(response => {
-
-})
-
-
-
-//console.log('=== Testing Finished ===');
