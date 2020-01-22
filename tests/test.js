e0ce614aca3bd9029ba4e235846ecc81fcd4f9cd
Nektarios <nek.liakis@gmail.com>
2020-01-22 13:38:41 +0200

    Added filters at getCompetitionsStandings

--- tests/test.js
+++ tests/test.js
@@ -3,5 +3,14 @@ require('dotenv').config();
 const FootballDataApi = require('../lib/FootballDataApi');
 const util = require('util')
 
-const footballDataApiKey = process.env.FOOTBALL_DATA_APIKEY;
+const footballDataApiKey = process.env.FOOTBALLDATA_APIKEY;
 const fd = new FootballDataApi(footballDataApiKey)
+
+
+fd.getCompetitions()
+	.then(res => {
+		console.info(JSON.stringify(res, null, 4));
+	})
+	.catch(err => {
+		console.error(JSON.stringify(err, null, 4));
+	})
